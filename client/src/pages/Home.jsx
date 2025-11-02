6adeeb9be2b1ea04daea133e47ca016d5e98eb9d
Anant Gautam <anantgautam88@gmail.com>
2025-11-02 09:36:01 +0530

    Fix import casing for Testimonial component

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Navbar from '../components/Navbar'
 import Hero from '../components/Hero'
 import Aitools from '../components/Aitools'
-import Testimonial from '../components/testimonial'
+import Testimonial from '../components/Testimonial'
 import Plan from '../components/Plan'
 import Footer from '../components/Footer'
 
